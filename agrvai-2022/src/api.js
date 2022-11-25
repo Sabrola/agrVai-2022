ceea275a66cc4bb158b0220574d087f180ac5a33
Sabrola <sabrinapezzoni@gmail.com>
2022-11-25 14:10:38 +0000

    25/11 - cookies

--- agrvai-2022/src/api.js
+++ agrvai-2022/src/api.js
@@ -3,7 +3,13 @@ import axios from 'axios'
 const api = axios.create({
     // Precisa terminar com /
     baseURL: 'https://agoravai-fausto.onrender.com/',
-    timeout: 5000  
+    timeout: 5000,
+    //Envia os cookies seguros $de volta a todas as requisições
+    credentials: true
+
+    /* headers: {
+        'x-access-token': window.sessionStorage.getItem('app-data')
+    } */  
 })
 
 export default api
